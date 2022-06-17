4ba07b2398a9541a4ff3bd7805c92c63319172df
vonrimS <mr.mytab@gmail.com>
2022-06-17 15:13:28 +0200

    random picker from dict

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from tkinter import *
+import pandas
+import random
 
 
 BG_COLOR = "#B1DDC6"
+
+data = pandas.read_csv('data/top5000.csv')
+to_learn = data.to_dict(orient="records")
+# print(to_learn)
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+    # print(current_card["French"])
 
 
 # ----- GUI SETUP -----
@@ -15,18 +27,20 @@ app_window.eval('tk::PlaceWindow %s center' % app_window.winfo_pathname(app_wind
 canvas = Canvas(width=800, height=500, bg=BG_COLOR, highlightthickness=0)
 card_img = PhotoImage(file="card.png")
 canvas.create_image(408, 250, image=card_img)
-canvas.create_text(400, 150, text="Title", font=('Ariel', 40, 'italic'))
-canvas.create_text(400, 270, text="Word", font=('Ariel', 60, 'bold'))
+card_title = canvas.create_text(400, 150, text="", font=('Ariel', 40, 'italic'))
+card_word = canvas.create_text(400, 270, text="", font=('Ariel', 60, 'bold'))
 
 canvas.grid(row=0, column=0, columnspan=3)
 
 wrong = PhotoImage(file="false.png")
-nok_button = Button(image=wrong, bg=BG_COLOR, activebackground=BG_COLOR, borderwidth=0)
+nok_button = Button(image=wrong, bg=BG_COLOR, activebackground=BG_COLOR, borderwidth=0, command=next_card)
 nok_button.grid(row=1, column=0, sticky='n')
 
 right = PhotoImage(file="true.png")
-ok_button = Button(image=right, bg=BG_COLOR, activebackground=BG_COLOR, borderwidth=0)
+ok_button = Button(image=right, bg=BG_COLOR, activebackground=BG_COLOR, borderwidth=0, command=next_card)
 ok_button.grid(row=1, column=2, sticky='n')
+
+next_card()
 
 app_window.mainloop()
 
